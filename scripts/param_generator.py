0400d038df161b9e1646fb34b8d168cf853fd689
adrianomcr <adrianomcr18@gmail.com>
2023-09-01 03:06:50 -0400

    adding parameter related scripts and a parameter file

---
+++ scripts/param_generator.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+import json
+
+def set_parameter(key, value):
+    parameters = load_parameters()
+    parameters[key] = value
+    save_parameters(parameters)
+
+def get_parameter(key):
+    parameters = load_parameters()
+    return parameters.get(key)
+
+def load_parameters():
+    try:
+        with open(PARAM_FILE, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+def save_parameters(parameters):
+    with open(PARAM_FILE, "w") as file:
+        json.dump(parameters, file, indent=4)
+
+PARAM_FILE = "/home/adrianomcr/simulation_ws/src/px4sim/config/sim_params.json"
+
+
+data = {}
+
+# Environmental params
+data["ENV_PRES_SEA"] ={
+    "description":  "Sea level standard atmospheric pressure [hPa]",
+    "value":        1013.25,
+    "default":      1013.25,
+    "type":         "float",
+    "unit":         "[hectopascal]"}
+data["ENV_GRAVITY"] = {
+    "description":   "Earth-surface gravitational acceleration [m/(s*s)]",
+    "value":         9.81,
+    "default":       9.81,
+    "type":          "float",
+    "unit":          "[meters]/([second]*[second])"}
+data["ENV_MOL_MAS"] = {
+    "description":   "Molar mass of dry air [kg/mol]",
+    "value":         0.02896968,
+    "default":       0.02896968,
+    "type":          "float",
+    "unit":          "[kilograms]/[mol]"}
+data["ENV_TMP_SES"] = {
+    "description":   "Sea level standard temperature [K]",
+    "value":         288.16,
+    "default":       288.16,
+    "type":          "float",
+    "unit":          "[Kelvin]"}
+data["ENV_GAS_CTE"] = {
+    "description":   "Universal gas constant [J/(mol*K)]",
+    "value":         8.314462618,
+    "default":       8.314462618,
+    "type":          "float",
+    "unit":          "[Joules]/([mol][Kelvin])"}
+
+# Dynamics params
+data["DYN_MASS"] = {
+    "description":   "Total mass of the vehicle [kg]",
+    "value":         2.0,
+    "default":       2.0,
+    "type":          "float",
+    "unit":          "[kilograms]"}
+data["DYN_DRAG_V"] = {
+    "description":   "Drag coefficiet for linear motion [N*s/m]",
+    "value":         0.2,
+    "default":       0.2,
+    "type":          "float",
+    "unit":          "[Newtons]*[seconds]/[meters]"}
+data["DYN_DRAG_W"] = {
+    "description":   "Drag coefficiet for angular motion [N*s/rad]",
+    "value":         0.02,
+    "default":       0.02,
+    "type":          "float",
+    "unit":          "[Newtons]*[seconds]/[radians]"}
+data["DYN_J_XX"] = {
+    "description":   "Moment of inertion around the x axis [kg*m*m]",
+    "value":         0.07625,
+    "default":       0.07625,
+    "type":          "float",
+    "unit":          "[kilogram]*[meter]*[meter]"}
+data["DYN_J_YY"] = {
+    "description":   "Moment of inertion around the y axis [kg*m*m]",
+    "value":         0.077812,
+    "default":       0.077812,
+    "type":          "float",
+    "unit":          "[kilogram]*[meter]*[meter]"}
+data["DYN_J_ZZ"] = {
+    "description":   "Moment of inertion around the z axis [kg*m*m]",
+    "value":         0.1060739,
+    "default":       0.1060739,
+    "type":          "float",
+    "unit":          "[kilogram]*[meter]*[meter]"}
+data["DYN_WIND_E"] = {
+    "description":   "Constant air speed in the East direction [m/s]",
+    "value":         0.0,
+    "default":       0.0,
+    "type":          "float",
+    "unit":          "[meter]/[secong]"}
+data["DYN_WIND_N"] = {
+    "description":   "Constant air speed in the North direction [m/s]",
+    "value":         0.0,
+    "default":       0.0,
+    "type":          "float",
+    "unit":          "[meter]/[secong]"}
+data["DYN_WIND_U"] = {
+    "description":   "Constant avertical air speed pointing up [m/s]",
+    "value":         0.0,
+    "default":       0.0,
+    "type":          "float",
+    "unit":          "[meter]/[secong]"}
+
+
+
+
+
+
+save_parameters(data)
+# pressure_sea = 1013.25 # Sea level standard atmospheric pressure [hPa]
+# g = 9.80665 # Earth-surface gravitational acceleration [m/ss]
+# M = 0.02896968 # Molar mass of dry air [kg/mol]
+# T0 = 288.16 # Sea level standard temperature [K]
+# R0 = 8.314462618 # Universal gas constant [J/(mol*K)]
+# C_bar = (T0*R0)/(g*M)
+
+
---
+++ scripts/param_gui.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import tkinter as tk
+from tkinter import filedialog, ttk
+import json
+
+class JsonViewerApp:
+    def __init__(self, root):
+        self.root = root        
+        self.root.title("JSON Viewer")
+        
+        self.load_button = tk.Button(root, text="Load JSON File", command=self.load_json)
+        self.load_button.pack(pady=10, side=tk.LEFT)
+
+        self.save_button = tk.Button(root, text="Save", command=self.save_json)
+        self.save_button.pack(pady=10, side=tk.LEFT)
+
+        self.saveas_button = tk.Button(root, text="Save As", command=self.saveas_json)
+        self.saveas_button.pack(pady=10, side=tk.LEFT)
+        
+
+        # self.load_button = tk.Button(root, text="Load JSON File", command=self.load_json)
+        # self.load_button.pack(pady=10, side=tk.LEFT)  # Placed on the left
+        
+        # self.set_button = tk.Button(root, text="Set", command=self.set_value)
+        # self.set_button.pack(pady=10, side=tk.LEFT)  # Placed on the left
+        
+        # self.save_button = tk.Button(root, text="Save As", command=self.save_json)
+        # self.save_button.pack(pady=10, side=tk.LEFT)  # Placed on the left
+        
+
+        # self.tree = ttk.Treeview(root, columns=("Description", "Value", "Default", "Type", "Unit"))
+        # self.tree.heading("#1", text="Description")
+        # self.tree.heading("#2", text="Value")
+        # self.tree.heading("#3", text="Default")
+        # self.tree.heading("#4", text="Type")
+        # self.tree.heading("#5", text="Unit")
+        self.tree = ttk.Treeview(root, columns=("Value", "Description", "Unit"))
+        self.tree.heading("#1", text="Value")
+        self.tree.heading("#2", text="Description")
+        self.tree.heading("#3", text="Unit")
+
+        self.tree.pack(side=tk.LEFT, padx=10)
+        
+        self.details_frame = tk.Frame(root)
+        self.details_frame.pack(side=tk.LEFT, padx=10)
+        
+        self.parameter_label = tk.Label(self.details_frame, text="Parameter Details:")
+        self.parameter_label.pack(pady=10)
+        
+        self.description_label = tk.Label(self.details_frame, text="Description:")
+        self.description_label.pack()
+        
+        # Create an editable Entry widget for the "Value" field
+        self.value_entry = tk.Entry(self.details_frame)
+        self.value_entry.pack()
+        
+        self.default_label = tk.Label(self.details_frame, text="Default:")
+        self.default_label.pack()
+        
+        self.type_label = tk.Label(self.details_frame, text="Type:")
+        self.type_label.pack()
+        
+        self.unit_label = tk.Label(self.details_frame, text="Unit:")
+        self.unit_label.pack()
+        
+        # Add a "Set" button to save the edited value
+        self.set_button = tk.Button(self.details_frame, text="Set", command=self.set_value)
+        self.set_button.pack(pady=10)
+
+        self.file_path = False
+        
+        self.data = {}  # Store loaded JSON data
+        
+    def load_json(self):
+        path = filedialog.askopenfilename(filetypes=[("JSON Files", "*.json")])
+        if path:
+            self.file_path = path
+            with open(self.file_path, "r") as json_file:
+                self.data = json.load(json_file)
+            
+            self.populate_tree()
+            
+    def populate_tree(self):
+        self.tree.delete(*self.tree.get_children())
+        for param, details in self.data.items():
+            description = details["description"]
+            value = details["value"]
+            # default = details["default"]
+            # data_type = details["type"]
+            unit = details["unit"]
+            
+            # self.tree.insert("", "end", text=param, values=(description, value, default, data_type, unit))
+            self.tree.insert("", "end", text=param, values=(value, description, unit))
+
+
+    def update_tree(self,key,value):
+        for param, details in self.data.items():
+            description = details["description"]
+            value = details["value"]
+            # default = details["default"]
+            # data_type = details["type"]
+            unit = details["unit"]
+            
+            # self.tree.insert("", "end", text=param, values=(description, value, default, data_type, unit))
+            self.tree.insert("", "end", text=param, values=(value, description, unit))
+
+
+
+            
+    # def update_details(self, event):
+    #     # print('\33[91m',event,'\33[0m')
+    #     selected_item = self.tree.selection()[0]
+    #     selected_param = self.tree.item(selected_item, "text")
+    #     details = self.data[selected_param]
+        
+    #     self.description_label.config(text=f"Description: {details['description']}")
+    #     self.value_entry.delete(0, tk.END)
+    #     self.value_entry.insert(0, details['value'])
+    #     self.default_label.config(text=f"Default: {details['default']}")
+    #     self.type_label.config(text=f"Type: {details['type']}")
+    #     self.unit_label.config(text=f"Unit: {details['unit']}")
+
+    def update_details(self, event):
+        selected_items = self.tree.selection()
+        print(selected_items)
+        if selected_items:
+            selected_item = selected_items[0]
+            selected_param = self.tree.item(selected_item, "text")
+            details = self.data[selected_param]
+
+            self.description_label.config(text=f"Description: {details['description']}")
+            self.value_entry.delete(0, tk.END)
+            self.value_entry.insert(0, details['value'])
+            self.default_label.config(text=f"Default: {details['default']}")
+            self.type_label.config(text=f"Type: {details['type']}")
+            self.unit_label.config(text=f"Unit: {details['unit']}")
+        else:
+            self.description_label.config(text="Description: ")
+            self.value_entry.delete(0, tk.END)
+            # self.value_entry.insert(0, details['value'])
+            self.default_label.config(text="Default: ")
+            self.type_label.config(text="Type: ")
+            self.unit_label.config(text="Unit: ")
+
+    def saveas_json(self):
+        path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON Files", "*.json")])
+        if path:
+            self.file_path = path
+            self.save_json()
+
+    def save_json(self):
+        if self.file_path:
+            with open(self.file_path, "w") as json_file:
+                json.dump(self.data, json_file, indent=4)
+        
+    # def set_value(self):
+    #     selected_item = self.tree.selection()[0]
+    #     selected_param = self.tree.item(selected_item, "text")
+    #     new_value = self.value_entry.get()
+        
+    #     # Update the value in self.data
+    #     self.data[selected_param]['value'] = new_value
+
+    #     # # Update the value in self.data
+    #     # self.data[selected_param]['value'] = new_value
+    #     # file_path = "./out.json"
+    #     # if file_path:
+    #     #     with open(file_path, "w") as json_file:
+    #     #         json.dump(self.data, json_file, indent=4)
+
+    #     # Update the Treeview
+    #     self.populate_tree()
+    #     # self.save_json()
+
+
+    def set_value(self):
+        selected_items = self.tree.selection()
+        if selected_items:
+            selected_item = self.tree.selection()[0]
+            selected_param = self.tree.item(selected_item, "text")
+            new_value = self.value_entry.get()
+            self.data[selected_param]['value'] = new_value
+            self.tree.item(selected_item, values=(new_value, self.data[selected_param]['description'], self.data[selected_param]['unit']))
+
+            
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = JsonViewerApp(root)
+    app.tree.bind("<<TreeviewSelect>>", app.update_details)
+    root.mainloop()
+
+
+
+# import tkinter as tk
+# from tkinter import filedialog, ttk
+# import json
+
+# class JsonViewerApp:
+#     def __init__(self, root):
+#         self.root = root
+#         self.root.title("JSON Viewer")
+        
+#         self.load_button = tk.Button(root, text="Load JSON File", command=self.load_json)
+#         self.load_button.pack(pady=10)
+        
+#         self.tree = ttk.Treeview(root, columns=("Description", "Value", "Default", "Type", "Unit"))
+#         self.tree.heading("#1", text="Description")
+#         self.tree.heading("#2", text="Value")
+#         self.tree.heading("#3", text="Default")
+#         self.tree.heading("#4", text="Type")
+#         self.tree.heading("#5", text="Unit")
+#         self.tree.pack(side=tk.LEFT, padx=10)
+        
+#         self.details_frame = tk.Frame(root)
+#         self.details_frame.pack(side=tk.LEFT, padx=10)
+        
+#         self.parameter_label = tk.Label(self.details_frame, text="Parameter Details:")
+#         self.parameter_label.pack(pady=10)
+        
+#         self.description_label = tk.Label(self.details_frame, text="Description:")
+#         self.description_label.pack()
+        
+#         self.value_label = tk.Label(self.details_frame, text="Value:")
+#         self.value_label.pack()
+        
+#         self.default_label = tk.Label(self.details_frame, text="Default:")
+#         self.default_label.pack()
+        
+#         self.type_label = tk.Label(self.details_frame, text="Type:")
+#         self.type_label.pack()
+        
+#         self.unit_label = tk.Label(self.details_frame, text="Unit:")
+#         self.unit_label.pack()
+        
+#         self.save_button = tk.Button(root, text="Save As", command=self.save_json)
+#         self.save_button.pack(pady=10)
+        
+#         self.data = {}  # Store loaded JSON data
+        
+#     def load_json(self):
+#         file_path = filedialog.askopenfilename(filetypes=[("JSON Files", "*.json")])
+#         if file_path:
+#             with open(file_path, "r") as json_file:
+#                 self.data = json.load(json_file)
+            
+#             self.populate_tree()
+            
+#     def populate_tree(self):
+#         self.tree.delete(*self.tree.get_children())
+#         for param, details in self.data.items():
+#             description = details["description"]
+#             value = details["value"]
+#             default = details["default"]
+#             data_type = details["type"]
+#             unit = details["unit"]
+            
+#             self.tree.insert("", "end", text=param, values=(description, value, default, data_type, unit))
+            
+#     def update_details(self, event):
+#         selected_item = self.tree.selection()[0]
+#         selected_param = self.tree.item(selected_item, "text")
+#         details = self.data[selected_param]
+        
+#         self.description_label.config(text=f"Description: {details['description']}")
+#         self.value_label.config(text=f"Value: {details['value']}")
+#         self.default_label.config(text=f"Default: {details['default']}")
+#         self.type_label.config(text=f"Type: {details['type']}")
+#         self.unit_label.config(text=f"Unit: {details['unit']}")
+        
+#     def save_json(self):
+#         file_path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON Files", "*.json")])
+#         if file_path:
+#             with open(file_path, "w") as json_file:
+#                 json.dump(self.data, json_file, indent=4)
+            
+# if __name__ == "__main__":
+#     root = tk.Tk()
+#     app = JsonViewerApp(root)
+#     app.tree.bind("<<TreeviewSelect>>", app.update_details)
+#     root.mainloop()
+
+
+
+
+# import tkinter as tk
+# from tkinter import filedialog
+# import json
+# from tkinter import ttk
+
+# class JsonViewerApp:
+#     def __init__(self, root):
+#         self.root = root
+#         self.root.title("JSON Viewer")
+        
+#         self.load_button = tk.Button(root, text="Load JSON File", command=self.load_json)
+#         self.load_button.pack(pady=10)
+        
+#         self.tree = ttk.Treeview(root, columns=("Description", "Value", "Default", "Type", "Unit"))
+#         self.tree.heading("#1", text="Description")
+#         self.tree.heading("#2", text="Value")
+#         self.tree.heading("#3", text="Default")
+#         self.tree.heading("#4", text="Type")
+#         self.tree.heading("#5", text="Unit")
+#         self.tree.pack()
+        
+#     def load_json(self):
+#         file_path = filedialog.askopenfilename(filetypes=[("JSON Files", "*.json")])
+#         if file_path:
+#             with open(file_path, "r") as json_file:
+#                 data = json.load(json_file)
+            
+#             for param, details in data.items():
+#                 description = details["description"]
+#                 value = details["value"]
+#                 default = details["default"]
+#                 data_type = details["type"]
+#                 unit = details["unit"]
+                
+#                 self.tree.insert("", "end", text=param, values=(description, value, default, data_type, unit))
+            
+# if __name__ == "__main__":
+#     root = tk.Tk()
+#     app = JsonViewerApp(root)
+#     root.mainloop()
+
+
+
+
+
+
