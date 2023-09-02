864c59bdcd2ec076c205a28356a855e596ce52de
adrianomcr <adrianomcr18@gmail.com>
2023-09-02 16:49:47 -0400

    updating parameters style

--- scripts/param_generator.py
+++ scripts/param_generator.py
@@ -32,91 +32,97 @@ data = {}
 
 # Environmental params
 data["ENV_PRES_SEA"] ={
-    "description":  "Sea level standard atmospheric pressure [hPa]",
+    "description":  "Sea level standard atmospheric pressure in hectopascal",
     "value":        1013.25,
     "default":      1013.25,
     "type":         "float",
-    "unit":         "[hectopascal]"}
+    "unit":         "[hPa]"}
 data["ENV_GRAVITY"] = {
-    "description":   "Earth-surface gravitational acceleration [m/(s*s)]",
+    "description":   "Earth-surface gravitational acceleration in meters per second square",
     "value":         9.81,
     "default":       9.81,
     "type":          "float",
-    "unit":          "[meters]/([second]*[second])"}
+    "unit":          "[m/(s*s)]"}
 data["ENV_MOL_MAS"] = {
-    "description":   "Molar mass of dry air [kg/mol]",
+    "description":   "Molar mass of dry air in kilograms per mol",
     "value":         0.02896968,
     "default":       0.02896968,
     "type":          "float",
-    "unit":          "[kilograms]/[mol]"}
+    "unit":          "[kg/mol]"}
 data["ENV_TMP_SES"] = {
-    "description":   "Sea level standard temperature [K]",
+    "description":   "Sea level standard temperature in Kelvin degrees",
     "value":         288.16,
     "default":       288.16,
     "type":          "float",
-    "unit":          "[Kelvin]"}
+    "unit":          "[K]"}
 data["ENV_GAS_CTE"] = {
-    "description":   "Universal gas constant [J/(mol*K)]",
+    "description":   "Universal gas constant in Joules per mol Kelvin",
     "value":         8.314462618,
     "default":       8.314462618,
     "type":          "float",
-    "unit":          "[Joules]/([mol][Kelvin])"}
+    "unit":          "[J/(mol*K)]"}
 
 # Dynamics params
 data["DYN_MASS"] = {
-    "description":   "Total mass of the vehicle [kg]",
+    "description":   "Total mass of the vehicle in kilograms",
     "value":         2.0,
     "default":       2.0,
     "type":          "float",
-    "unit":          "[kilograms]"}
+    "unit":          "[kg]"}
 data["DYN_DRAG_V"] = {
-    "description":   "Drag coefficiet for linear motion [N*s/m]",
+    "description":   "Drag coefficiet for linear motion in Newtons second per meter",
     "value":         0.2,
     "default":       0.2,
     "type":          "float",
-    "unit":          "[Newtons]*[seconds]/[meters]"}
+    "unit":          "[N*s/m]"}
 data["DYN_DRAG_W"] = {
-    "description":   "Drag coefficiet for angular motion [N*s/rad]",
+    "description":   "Drag coefficiet for angular motion Newtons seconds per radian",
     "value":         0.02,
     "default":       0.02,
     "type":          "float",
-    "unit":          "[Newtons]*[seconds]/[radians]"}
+    "unit":          "[N*s/rad]"}
 data["DYN_J_XX"] = {
-    "description":   "Moment of inertion around the x axis [kg*m*m]",
+    "description":   "Moment of inertion around the x axis in kilogram meter square",
     "value":         0.07625,
     "default":       0.07625,
     "type":          "float",
-    "unit":          "[kilogram]*[meter]*[meter]"}
+    "unit":          "[kg*m*m]"}
 data["DYN_J_YY"] = {
-    "description":   "Moment of inertion around the y axis [kg*m*m]",
+    "description":   "Moment of inertion around the y axis in kilogram meter square",
     "value":         0.077812,
     "default":       0.077812,
     "type":          "float",
-    "unit":          "[kilogram]*[meter]*[meter]"}
+    "unit":          "[kg*m*m]"}
 data["DYN_J_ZZ"] = {
-    "description":   "Moment of inertion around the z axis [kg*m*m]",
+    "description":   "Moment of inertion around the z axis in kilogram meter square",
     "value":         0.1060739,
     "default":       0.1060739,
     "type":          "float",
-    "unit":          "[kilogram]*[meter]*[meter]"}
+    "unit":          "[kg*m*m]"}
 data["DYN_WIND_E"] = {
-    "description":   "Constant air speed in the East direction [m/s]",
+    "description":   "Constant air speed in the East direction in meters per second",
     "value":         0.0,
     "default":       0.0,
     "type":          "float",
-    "unit":          "[meter]/[secong]"}
+    "unit":          "[m/s]"}
 data["DYN_WIND_N"] = {
-    "description":   "Constant air speed in the North direction [m/s]",
+    "description":   "Constant air speed in the North direction in meters per second",
     "value":         0.0,
     "default":       0.0,
     "type":          "float",
-    "unit":          "[meter]/[secong]"}
+    "unit":          "[m/s]"}
 data["DYN_WIND_U"] = {
-    "description":   "Constant avertical air speed pointing up [m/s]",
+    "description":   "Constant avertical air speed pointing up in meters per second",
     "value":         0.0,
     "default":       0.0,
     "type":          "float",
-    "unit":          "[meter]/[secong]"}
+    "unit":          "[m/s]"}
+data["TEST_PARAM"] = {
+    "description":   "Enable disable flag test",
+    "value":         True,
+    "default":       True,
+    "type":          "bool",
+    "unit":          "[]"}
 
 
 
